8886c06b6444141998c8d1ee827f6770c8591884
REAU <anaisreau@Anais.Reau>
2020-03-25 16:38:41 +0100

    maj

--- wilder.js
+++ wilder.js
@@ -1,9 +1,3 @@
-const http = require('http');
+const chalk = require('chalk');
 
-const server = http.createServer((request, response) => {
-response.writeHead(200);
-response.end('Hello World');
-});
-
-server.listen(3000);
-console.log('Adresse du serveur: http://localhost:3000');
+console.log(chalk.blue('sabrina ')+chalk.red('anthony ')+chalk.gray('helene ')+chalk.green('nadir '));
